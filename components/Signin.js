85a70725db9c8a8d2d16d074c02aab6b90b64fac
Chalanika <chalanikasewwandi@gmail.com>
2020-07-22 00:25:11 +0530

    updated signin page

--- components/Signin.js
+++ components/Signin.js
@@ -12,48 +12,58 @@ import {
 //import {Container,Contect,Header,Form} from 'native-base';
 import { Input, Block, Button } from "galio-framework";
 import firebase from "./firebase";
+import { ScrollView } from "react-native-gesture-handler";
 
-export default function Signin() {
+export default function Signin({ navigation }) {
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
 
   const onSignin = () => {
-    firebase
-      .auth()
-      .signInWithEmailAndPassword(email, password)
-      .then((userRef) => {
-        console.log(999, userRef);
-      });
+    try {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(email, password)
+        .then((userRef) => {
+          console.log(999, userRef);
+        });
+      navigation.navigate("ViewRecentNotifications");
+    } catch (error) {
+      console.log(error);
+    }
   };
   return (
     <View style={styles.container}>
-      <View style={styles.content}>
-        <Image
-          source={{
-            width: 400,
-            height: 300,
-            uri:
-              "https://img.freepik.com/free-vector/mobile-login-concept-illustration_114360-83.jpg?size=338&ext=jpg",
-          }}
-        />
-        <Input
-          placeholder="Email"
-          rounded
-          style={{ marginLeft: 5, marginRight: 5, marginTop: 5 }}
-          onChangeText={(val) => setEmail(val)}
-        />
-        <Input
-          placeholder="password"
-          password
-          viewPass
-          rounded
-          style={{ marginLeft: 5, marginRight: 5 }}
-          onChangeText={(val) => setPassword(val)}
-        />
-        <Button round uppercase color="info" onPress={() => onSignin()}>
-          Sign In
-        </Button>
-      </View>
+      <ScrollView>
+        <View style={styles.content}>
+          <Image
+            source={{
+              width: 400,
+              height: 300,
+              uri:
+                "https://img.freepik.com/free-vector/mobile-login-concept-illustration_114360-83.jpg?size=338&ext=jpg",
+            }}
+          />
+          <View style={{ width: "80%" }}>
+            <Input
+              placeholder="Email"
+              rounded
+              style={{ marginTop: 5 }}
+              onChangeText={(val) => setEmail(val)}
+            />
+            <Input
+              placeholder="password"
+              password
+              viewPass
+              rounded
+              style={{ marginBottom: 20 }}
+              onChangeText={(val) => setPassword(val)}
+            />
+          </View>
+          <Button round uppercase color="info" onPress={() => onSignin()}>
+            Sign In
+          </Button>
+        </View>
+      </ScrollView>
     </View>
   );
 }
@@ -64,12 +74,11 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
-
     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
 
   content: {
-    width: "80%",
+    width: "100%",
     alignItems: "center",
     justifyContent: "center",
   },
