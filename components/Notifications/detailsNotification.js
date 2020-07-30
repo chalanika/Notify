18b9796d4b76c5eb87937c9ec7601338670e3181
unknown <chalanikasewwandi@gmail.com>
2020-07-30 18:30:10 +0530

    finished viwing details of selected notification

--- components/Notifications/detailsNotification.js
+++ components/Notifications/detailsNotification.js
@@ -6,18 +6,26 @@ import {
     StatusBar,
     Platform,
     StyleSheet,
+    Image
 } from "react-native";
 import { Button, Input } from "galio-framework";
-import firebase from "../firebase";
 
 const DetailsNotification = ({ route, navigation }) => {
-    const { docId, title, description, date } = route.params;
-
-    console.log(666, docId, title, description, date);
+    const { title, description, date } = route.params;
     return (
-
         <View style={styles.container}>
+            <View style={styles.content}>
+                <View style={{ flexDirection: 'column', width: '100%', }} >
+                    <Text style={{ fontWeight: 'bold', width: '100%', fontSize: 30 }}>{title}</Text>
+                    <Text style={{ color: 'gray' ,fontSize:12}}>{new Date(parseInt(date * 1000)).toDateString()}{'   '}{new Date(parseInt(date * 1000)).toLocaleTimeString()}</Text>
+                </View>
+                <View style={{ marginTop: 20, marginRight: 20 }}>
+                    <Text style={{ fontSize: 16 }}>{description}</Text>
+                </View>
 
+
+
+            </View>
         </View>
     );
 };
@@ -25,17 +33,21 @@ const DetailsNotification = ({ route, navigation }) => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "#fff",
-        // alignItems: "center",
-        // justifyContent: "center",
+        backgroundColor: 'whitesmoke',
+        alignItems: "center",
+
         paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
     },
 
     content: {
-        width: "100%",
-        // alignItems: "center",
-        // justifyContent: "center",
-        flex: 1,
+        width: "80%",
+        backgroundColor: 'white',
+        paddingLeft: 10,
+        borderRadius: 10,
+        justifyContent: "center",
+        flexDirection: 'column',
+        alignItems: 'center',
+        marginTop: 40
     },
 });
 
