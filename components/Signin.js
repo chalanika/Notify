cb0b6c5139318c26b6de65a16035908e57d76792
unknown <chalanikasewwandi@gmail.com>
2020-08-01 11:42:11 +0530

    added validation for signin form

--- components/Signin.js
+++ components/Signin.js
@@ -15,23 +15,34 @@ import firebase from "./firebase";
 import { ScrollView } from "react-native-gesture-handler";
 
 export default function Signin({ navigation }) {
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
-
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error,setError] = useState();
+ 
   const onSignin = () => {
-    try {
       firebase
         .auth()
         .signInWithEmailAndPassword(email, password)
         .then((userRef) => {
-          //console.log(999,userRef);
-          navigation.navigate("ViewRecentNotifications");
+          if(userRef){
+            navigation.navigate("ViewRecentNotifications");
+          }
+        })
+        .catch(error=>{
+          console.log(error.code);
+          console.log(error.message);
+          error.code === 'auth/invalid-email'
+          ? setError('Please Enter Valid Email') 
+          : error.code === 'auth/user-not-found'
+          ? setError('You have been registered first')
+          : error.code === 'auth/too-many-requests'
+          ? setError('Too many unsuccessful login attempts. Please try again later.') 
+          : error.code === 'auth/wrong-password'
+          ? setError('The password is invalid') 
+          :setError('Something is wrong. Please try again later.')
         });
-     
-    } catch (error) {
-      console.log(error);
-    }
   };
+ 
   return (
     <View style={styles.container}>
       <ScrollView>
@@ -44,13 +55,16 @@ export default function Signin({ navigation }) {
                 "https://img.freepik.com/free-vector/mobile-login-concept-illustration_114360-83.jpg?size=338&ext=jpg",
             }}
           />
+          {error && <Text style={{color:'red',fontSize:14,margin:20,justifyContent:'center',textAlign:'center'}}>{error}</Text>}
           <View style={{ width: "80%" }}>
             <Input
               placeholder="Email"
               rounded
               style={{ marginTop: 5 }}
               onChangeText={(val) => setEmail(val)}
+              onFocus = {()=>setError(' ')}
             />
+            
             <Input
               placeholder="password"
               password
@@ -58,7 +72,9 @@ export default function Signin({ navigation }) {
               rounded
               style={{ marginBottom: 20 }}
               onChangeText={(val) => setPassword(val)}
+              onFocus = {()=>setError(' ')}
             />
+            
           </View>
           <Button round uppercase color="info" onPress={() => onSignin()}>
             Sign In
