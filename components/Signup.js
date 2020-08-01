f78d8f6ff9b3d326b3a07dcb362251f26090cb79
unknown <chalanikasewwandi@gmail.com>
2020-08-01 21:31:51 +0530

    added validation

--- components/Signup.js
+++ components/Signup.js
@@ -13,17 +13,22 @@ import firebase from "./firebase";
 import { ScrollView } from "react-native-gesture-handler";
 
 export default function Signup({ navigation }) {
-  const [name, setName] = useState();
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
-  const [position, setPosition] = useState();
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [position, setPosition] = useState('');
+  const [error, setError] = useState();
+  const [validName, setvalidName] = useState();
+  const [validEmail, setvalidEmail] = useState();
+  const [validPosition, setvalidPosition] = useState();
+  const [validPassword, setvalidPassword] = useState();
 
   const onSignup = () => {
-    try {
-      if (password.length < 6) {
-        alert("Please enter at least 6 characters");
-        return;
-      }
+    if (email === '') setvalidName('Name is required.')
+    if (name === '') setvalidEmail('Email is required.');
+    if (position === '') setvalidPosition('Position is required.');
+    if (password === '') setvalidPassword('Password is required.');
+    if (email !== '' && name !== '' && position !== '' && password !== '') {
       firebase
         .auth()
         .createUserWithEmailAndPassword(email, password)
@@ -42,11 +47,32 @@ export default function Signup({ navigation }) {
             });
             navigation.navigate("ViewRecentNotifications");
           }
+        }).catch(error => {
+          console.log(error.code);
+          console.log(error.message);
+          error.code === 'auth/invalid-email'
+            ? setError('Email Address is invalid')
+            : error.code === 'auth/weak-password'
+              ? setError('The password must be 6 characters long or more.')
+              : error.code === 'auth/email-already-in-use'
+                ? setError('The email address is already in use by another account.')
+                : error.code === 'auth/too-many-requests'
+                  ? setError('Too many unsuccessful login attempts. Please try again later.')
+                  : setError('Something is wrong. Please try again later.')
+
         });
-    } catch (error) {
-      console.log(error);
+
     }
+
   };
+  const handleError = ()=>{
+    setError(' ');
+     setvalidPosition(' ');
+     setvalidName(' ');
+     setvalidEmail(' ');
+     setvalidPassword(' ');
+  }
+
   return (
     <View style={styles.container}>
       <ScrollView>
@@ -59,27 +85,32 @@ export default function Signup({ navigation }) {
                 "https://img.freepik.com/free-vector/account-log-page_41910-263.jpg?size=626&ext=jpg",
             }}
           />
-
+          {error && <Text style={{ color: 'red', fontSize: 14, margin: 10, justifyContent: 'center', textAlign: 'center' }}>{error}</Text>}
           <View style={{ width: "80%" }}>
             <Input
               placeholder="Name"
               rounded
               style={{ marginLeft: 5, marginRight: 5, marginTop: 5 }}
               onChangeText={(val) => setName(val)}
+              onFocus={() => handleError()}
             />
+            {validName && <Text style={{ color: 'red', fontSize: 12, paddingLeft: 10 }}>{validName}</Text>}
             <Input
               placeholder="Email"
               rounded
               style={{ marginLeft: 5, marginRight: 5 }}
               onChangeText={(val) => setEmail(val)}
+              onFocus={() => handleError()}
             />
+            {validEmail && <Text style={{ color: 'red', fontSize: 12, paddingLeft: 10 }}>{validEmail}</Text>}
             <Input
               placeholder="Position"
               rounded
               style={{ marginLeft: 5, marginRight: 5 }}
               onChangeText={(val) => setPosition(val)}
+              onFocus={() => handleError()}
             />
-
+            {validPosition && <Text style={{ color: 'red', fontSize: 12, paddingLeft: 10 }}>{validPosition}</Text>}
             <Input
               placeholder="password"
               password
@@ -87,7 +118,9 @@ export default function Signup({ navigation }) {
               rounded
               style={{ marginLeft: 5, marginRight: 5 }}
               onChangeText={(val) => setPassword(val)}
+              onFocus={() => handleError()}
             />
+            {validPassword && <Text style={{ color: 'red', fontSize: 12, paddingLeft: 10 }}>{validPassword}</Text>}
           </View>
           <Button
             round
